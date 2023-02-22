45ec2bc11d5ae15106c8bd887dd3b8fa4c76a931
Julio Tambani <juliotambani@gmail.com>
2023-02-22 11:04:00 -0300

    Questão número 4 sobre o cálculo do salário descontando INSS e IR foi realizado com sucesso!

---
+++ 01_fundamentos/03_introducao_javascript/01_javascript_primeiros_passos/04_index.js
@@ -0,0 +1,31 @@
+let salarioBruto = 3000;
+let aliquotaInss;
+let ir;
+
+if (salarioBruto > 0 && salarioBruto <= 1556.94) {
+    aliquotaInss = salarioBruto * 0.08;
+} else if (salarioBruto >= 1566.94 && salarioBruto <= 2594.92) {
+    aliquotaInss = salarioBruto * 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    aliquotaInss = salarioBruto * 0.11;
+} else {
+    aliquotaInss = 570.88;
+}
+
+let salarioBase = salarioBruto - aliquotaInss;
+
+if (salarioBase > 0 && salarioBase <= 1903.98) {
+    ir = 0;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+    ir = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    ir = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    ir = (salarioBase * 0.225) - 636.13;   
+} else {
+    let ir = (salarioBruto * 0.275) - 869.36;
+};
+
+let salarioLiquido = salarioBase - ir;
+
+console.log(salarioLiquido)
