960ecfcb7679b1c938cc8d0ec053443fe836f072
Julio Tambani <juliotambani@gmail.com>
2023-02-23 13:04:40 -0300

    Finalizado questão 3,média aritmética dos valores contidos no array.

--- 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/index.js
+++ 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/index.js
@@ -5,14 +5,14 @@ Considere esse array para realizar os próximos exercícios.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 1) Percorra o array imprimindo todos os valores contidos nele com a função console.log().
-*/
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index <= 9; index = index + 1) {
     console.log('Questão 1: ' + numbers[index]);
 }
-/*
-2) Some todos os valores contidos no array e imprima o resultado.*/
+
+2) Some todos os valores contidos no array e imprima o resultado.
 let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
 
@@ -21,10 +21,22 @@ for (let index = 0; index < numeros.length; index += 1) {
 }
 console.log('Questão 2: ' + resultado);
 
+*/
 /*
-3) Calcule e imprima a média aritmética dos valores contidos no array.
-4) A média aritmética é o resultado da soma de todos os elementos dividido pelo 
-número total de elementos.
+3) Calcule e imprima a média aritmética dos valores contidos no array. 
+A média aritmética é o resultado da soma de todos os elementos dividido pelo 
+número total de elementos.*/
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let index = 0; index < numeros.length; index += 1) {
+    resultado += numeros[index];
+}
+console.log('Questão 3: '+resultado/numeros.length);
+
+
+
+/*
 5) Com base no código que acabou de gerar, faça com que: caso o valor final seja 
 maior que 20, imprima a mensagem “Valor maior que 20”; e, caso não seja maior que 20, 
 6) imprima a mensagem “Valor menor ou igual a 20”.
