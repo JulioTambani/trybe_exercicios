81518b4e712bcbd82af5d980ab273effb544a294
Julio Tambani <juliotambani@gmail.com>
2023-02-24 09:43:56 -0300

    Questão 6 realizado

--- 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/index.js
+++ 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/index.js
@@ -67,6 +67,21 @@ console.log(maiorNumero);
 6) Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista 
 nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 */
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    resultado += 1;
+  }
+}
+
+if (resultado === 0) {
+  console.log('Não tem valor ímpar');
+} else {
+  console.log(resultado);
+}
+
 
 /*
 9) Utilizando for, descubra o menor valor contido no array e imprima-o.
