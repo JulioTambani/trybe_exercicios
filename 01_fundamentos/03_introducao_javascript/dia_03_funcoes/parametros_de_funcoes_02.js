bc28df49abc2bb90f83ddbdb3de1fa025b169e4d
Julio Tambani <juliotambani@gmail.com>
2023-03-01 09:39:35 -0300

    Desenvolvendoa sistema de excluir cliente já cadastrado.

--- 01_fundamentos/03_introducao_javascript/dia_03_funcoes/parametros_de_funcoes_02.js
+++ 01_fundamentos/03_introducao_javascript/dia_03_funcoes/parametros_de_funcoes_02.js
@@ -12,4 +12,14 @@ function adicionaCliente(cliente) {
 
 console.log(adicionaCliente(true)); // O parâmetro passado deve ser do tipo "string"
 console.log(adicionaCliente('Joaquim')); // Cliente adicionada(o) com sucesso
-console.log(clientesTrybeBank); // [ 'Ada', 'John', 'Gus', 'Joaquim' ]
+console.log(clientesTrybeBank); // [ 'Ada', 'John', 'Gus', 'Joaquim' ]let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function removeCliente(cliente) {
+  if (typeof cliente === 'string') {
+    for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+  
+    }
+  } else {
+    return 'O parâmetro passado deve ser do tipo "string"';
+  }
+}
