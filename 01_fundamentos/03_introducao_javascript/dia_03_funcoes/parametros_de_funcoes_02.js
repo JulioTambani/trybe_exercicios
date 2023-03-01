af11dad7d0effff699d4904535f5739f968efbb3
Julio Tambani <juliotambani@gmail.com>
2023-03-01 09:37:07 -0300

    Verificar se o parâmetrorecebido é uma string.

--- 01_fundamentos/03_introducao_javascript/dia_03_funcoes/parametros_de_funcoes_02.js
+++ 01_fundamentos/03_introducao_javascript/dia_03_funcoes/parametros_de_funcoes_02.js
@@ -1,5 +1,9 @@
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
 function adicionaCliente(cliente) {
+  if (typeof cliente === 'string') {
 
+  } else {
+    return 'O parâmetro passado deve ser do tipo "string"';
+  }
 }
