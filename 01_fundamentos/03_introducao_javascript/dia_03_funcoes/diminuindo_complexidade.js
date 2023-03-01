4fc84ada433f23cbfcd62c1e32b423a858bf623c
Julio Tambani <juliotambani@gmail.com>
2023-03-01 10:05:17 -0300

    Primeiro passo: Defina a função validaCliente, que é responsável por validar se o parâmetro é do tipo string.

--- 01_fundamentos/03_introducao_javascript/dia_03_funcoes/diminuindo_complexidade.js
+++ 01_fundamentos/03_introducao_javascript/dia_03_funcoes/diminuindo_complexidade.js
@@ -1,22 +1,11 @@
 // Refatore a função removeCliente para diminuir sua complexidade e quebrá-la em funções menores.
 
-let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+// let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
-function removeCliente(cliente) {
-  if (typeof cliente === 'string') {
-    let clienteEncontrado = false;
-    for (let index = 0; index < clientesTrybeBank.length; index += 1) {
-      if (cliente === clientesTrybeBank[index]) {
-        clientesTrybeBank.splice(index, 1);
-        clienteEncontrado = true;
-        return 'Cliente excluída(o) com sucesso.';
-      }
+function validaCliente(cliente) {
+    if (typeof cliente !== 'string') {
+      return 'O parâmetro passado deve ser do tipo "string"!';
+    } else {
+      return true;
     }
-
-    if (clienteEncontrado === false) {
-      return 'Cliente não encontrado'
-    }
-  } else {
-    return 'O parâmetro passado deve ser do tipo "string"!';
   }
-};
