df93cc3d1b957003c28897b3a2f1c9956178abe0
Julio Tambani <juliotambani@gmail.com>
2023-03-02 19:28:02 -0300

    Foi acessado  a chave bestInTheWorld por meio da sintaxe meuObjeto[chave] e feito um console.log no seguinte formato: A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes.

--- 01_fundamentos/03_introducao_javascript/dia_04_objetos/exercicios_de_fixacao.js
+++ 01_fundamentos/03_introducao_javascript/dia_04_objetos/exercicios_de_fixacao.js
@@ -1,3 +1,19 @@
+// console.log(player['name']);
+// console.log(player['lastName']);
+// console.log(player['age']);
+
+
+//Faça os exercícios de fixação a seguir para consolidar os conhecimentos adquiridos! 
+
+// 1) Crie uma variável player e atribua a ela um objeto que contenha as variáveis listadas na sequência.
+//    Acesse as chaves name, lastName e age por meio da sintaxe meuObjeto.chave e concatene as informações para imprimir no console uma mensagem no seguinte formato: “A jogadora Marta Silva tem 34 anos de idade”
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+//R:
 let player = {
     name: 'Marta',
     lastName: 'Silva',
@@ -5,11 +21,19 @@ let player = {
     medals: { golden: 2, silver: 3 }
 
 };
-// console.log(player['name']);
-// console.log(player['lastName']);
-// console.log(player['age']);
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
 
+
+// 2)Adicione ao objeto a chave bestInTheWorld por meio da sintaxe meuObjeto.chave = valor e atribua a essa chave um array que contenha as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+// R:
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]; // Ao tentar acessar uma chave inexistente, o JavaScript automaticamente criará essa chave.
+// console.log(player); //aqui para fazer o teste e imprimir a resolução.
 
-console.log(player); //aqui para fazer o teste e imprimir a resolução.
+
+// 3) Acesse a chave bestInTheWorld por meio da sintaxe meuObjeto['chave'] e faça um console.log no seguinte formato: “A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes”.
+
+//R:
+
+//console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].lenght + ' vezes. '); // esse que eu fiz dá indefinido o numero  de vezes.
+  console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
