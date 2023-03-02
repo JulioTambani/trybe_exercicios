7f5f08a8ac5a51bee1c30cb520e9956526e5146d
Julio Tambani <juliotambani@gmail.com>
2023-03-02 19:09:08 -0300

    Foi adicionado um objeto a chave  bestInTheWorld por meio da sintaxe meuObjeto.chave = valor e atribuido a essa chave um array que contem as datas em que a jogadora Marta foi considerada a melhor do mundo.

--- 01_fundamentos/03_introducao_javascript/dia_04_objetos/exercicios_de_fixacao.js
+++ 01_fundamentos/03_introducao_javascript/dia_04_objetos/exercicios_de_fixacao.js
@@ -9,3 +9,7 @@ let player = {
 // console.log(player['lastName']);
 // console.log(player['age']);
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]; // Ao tentar acessar uma chave inexistente, o JavaScript automaticamente criará essa chave.
+
+console.log(player); //aqui para fazer o teste e imprimir a resolução.
