59546d1a4dd4293b6c30c5a0c991e717b89c2cf4
Julio Tambani <juliotambani@gmail.com>
2023-03-03 16:01:43 -0300

    Exercício da mega-sena com loop, array e for.REVISAO DA AULA AO VIVO

--- 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/rascunho.js
+++ 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/rascunho.js
@@ -1,5 +1,11 @@
-let lotteryNumbers = [24, 32, 12, 8, 45, 57];
+let lotteryNumbers = [];
 
-for (let index = 0; index < lotteryNumbers.length; index += 1){
-    console.log(`Número ${index} = ${lotteryNumbers[index]}`);
+for (let index = 0; index < 6; index += 1) { // vai rodar 6x pq meu array está vazio
+    //   console.log(`Número ${index} = ${lotteryNumbers[index]}`);
+    //   console.log(Math.ceil(Math.random() * 60)); // arredonda para cima usando o Math.CEIL
+    //   console.log(Math.floor(Math.random() * 60)); // arredonda para cima usando o Math.FLOOR.
+
+
+    lotteryNumbers.push(Math.ceil(Math.random() * 60)); // Vou chamar meu lotteryNumbers que é um array vazio usando o push, ou seja, para cada fez que o meu for executar euirei gerar um número aleatório  "(Math.random()*60)"  vou arredondar para cima "Math.ceil(Math.random()*60)" e colocar no meu array "lotteryNumbers".
 }
+    console.log(lotteryNumbers);
