0ae9d1f482b39a87389d85637fc1abbb00c97601
Julio Tambani <juliotambani@gmail.com>
2023-03-03 10:28:55 -0300

    Tipos de arrow functions

---
+++ 01_fundamentos/03_introducao_javascript/dia_05_javascript_es6_let_const_arrow_functions_e_template_literals/00_arrow_function_exemplo.js
@@ -0,0 +1,17 @@
+// const soma=function (num1,num2){
+//     return num1 +num2;
+// }
+// console.log(soma(2,3));
+
+// Exemplo 1
+
+// const soma=(num1,num2)=>{
+//     return num1 +num2;
+// }
+// console.log(soma(2,3));
+
+// Exemplo 2
+
+ const soma=(num1,num2)=>num1 +num2;
+ 
+ console.log(soma(2,3));
