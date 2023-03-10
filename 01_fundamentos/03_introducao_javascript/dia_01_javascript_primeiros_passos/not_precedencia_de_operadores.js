31de26fac17a19a7c9a4cf744b232a1a61b960fe
Julio Tambani <juliotambani@gmail.com>
2023-03-10 07:02:26 -0300

    finalizado o módulo

---
+++ 01_fundamentos/03_introducao_javascript/dia_01_javascript_primeiros_passos/not_precedencia_de_operadores.js
@@ -0,0 +1,6 @@
+//Esse código deve retornar true, não é? Afinal, 2 + 2 resultar em 4 é uma declaração verdadeira. E se adicionarmos o operador NOT antes dessa declaração?
+
+console.log((2 + 2) === 4);
+// true
+console.log(!(2 + 2) === 4);
+// false
