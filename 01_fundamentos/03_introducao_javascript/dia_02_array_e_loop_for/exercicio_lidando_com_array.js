2f59bf4d1291aa73aaaed5f1d9218dfb360a31ff
Julio Tambani <juliotambani@gmail.com>
2023-03-10 18:12:18 -0300

    realização dos exercicios

--- 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/exercicio_lidando_com_array.js
+++ 01_fundamentos/03_introducao_javascript/dia_02_array_e_loop_for/exercicio_lidando_com_array.js
@@ -35,18 +35,55 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Utilizando for, descubra o maior valor contido no array e imprima-o. Rsp:100
-let maiorNumero = numbers[0];
-for (let index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] > maiorNumero) {
-        maiorNumero = numbers[index];
-    }
-}
 
-console.log(maiorNumero);
+// let maiorNumero = numbers[0];
+// for (let index = 1; index < numbers.length; index += 1) {
+//     if (numbers[index] > maiorNumero) {
+//         maiorNumero = numbers[index];
+//     }
+// }
+
+// console.log(maiorNumero);
+
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+
+// let resultado=0;
+// for (let index=0;index<numbers.length;index+=1){
+//     if(numbers[index]%2!==0){
+//         resultado+=1;
+//     }
+// }
+// if(resultado===0){
+//     console.log('nenhum valor encontrado');
+// }else{
+//     console.log(resultado);
+// }
 
 // Utilizando for, descubra o menor valor contido no array e imprima-o.
 
+// let menorNumero = numbers[0];
+// for (let index = 1; index < numbers.length; index += 1) {
+//     if (numbers[index] < menorNumero) {
+//         menorNumero = numbers[index];
+//     }
+// }
+// console.log(menorNumero);
+
 // Utilizando for, crie um array que vá de 1 a 25 e imprima o resultado.
 
+// let numeros=[];
+// for (let index = 0; index <= 25; index += 1) {
+//     numeros.push(index);
+// }
+// console.log(numeros);
+
 // Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+let numeros = [];
+for (let index = 0; index <= 25; index += 1) {
+    numeros.push(index);
+}
+
+for (let index = 0; index <= 25; index += 1) {
+    console.log(numeros[index]/2);
+}
