065469219ac8f495818f42270648a0663aa4be7e
Julio Tambani <juliotambani@gmail.com>
2023-03-12 12:13:01 -0300

    inicio aula de DOM e seletores

---
+++ 01_fundamentos/04_java_script_dom_eventos_web_storage/teste1.js
@@ -0,0 +1,7 @@
+// aula do video com o lucas. 
+
+console.log(document.getElementById("teste2").innerHTML);
+
+console.log(document.getElementById("teste2").innerText);
+
+console.log(document.getElementById("teste2").style);
