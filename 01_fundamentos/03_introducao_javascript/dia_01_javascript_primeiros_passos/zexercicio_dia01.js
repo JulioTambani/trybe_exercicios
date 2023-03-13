ce29b464ebf6a47a3af267e56e6317e1f8c5f3c3
Julio Tambani <juliotambani@gmail.com>
2023-03-13 15:14:36 -0300

    exercicios primeiro dia

---
+++ 01_fundamentos/03_introducao_javascript/dia_01_javascript_primeiros_passos/zexercicio_dia01.js
@@ -0,0 +1,84 @@
+// 1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+// let a = 'valor a';
+// let b = 'valor b';
+
+// console.log(a);
+// console.log(b);
+
+// 2 - Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+
+// let num1=30;
+// let num2=20;
+// if (num1>num2){
+//     console.log(`O maior valor é ${num1}`);
+// }else{
+//     console.log(`O maior valor é ${num2}`);
+// };
+
+// 3 - Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+// let num1 = 10;
+// let num2 = 40;
+// let num3 = 30;
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log(`Numero ${num1} maior que ${num2} e ${num3}`);
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log(`Numero ${num2} maior que ${num1} e ${num3}`);
+// } else if (num3 > num1 && num3 > num2) {
+//     console.log(`Numero ${num3} maior que ${num1} e ${num3}`);
+// }
+
+//  4- Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne: “positive”, se esse valor for positivo; “negative”, se esse valor for negativo, e “zero”, caso esse valor não seja nem positivo nem negativo.
+
+// let number=0;
+
+// if (number>0){
+//     console.log(`Número ${number} é um número positivo`);
+// }else if (number<0){
+//     console.log(`Número ${number} é um número negativo`);
+// }else if (number===0){
+//     console.log(`Número ${number}, o valor é zero`);
+// }
+
+// 5 - Utilize if/else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+
+// let ladoA = 60;
+// let ladoB = 60;
+// let ladoC = 70;
+
+// if (ladoA + ladoB + ladoC === 180) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+//     console.log(`Algum angulo inválido`);
+// }
+
+// 6 - Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+
+// 6.1 - Utilize if/else para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+
+// let pecaXadrez= 'peao';
+
+// if (pecaXadrez==='peao'){
+//     console.log(`Voce escolheu o peão, a sua peça pode andar na diagonal, uma peça por vez `);
+// }else{
+//     console.log("vc escolheu uma peça diferente");
+//}
+
+// // 7 - Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga as seguintes regras:
+
+// let nota = 72;
+
+// if (nota >= 90) {
+//     console.log(`${nota} corresponde um A`);
+// } else if (nota >= 80) {
+//     console.log(`${nota} corresponde um B`);
+// } else if (nota >= 70) {
+//     console.log(`${nota} corresponde um C`);
+// } else if (nota >= 60) {
+//     console.log(`${nota} corresponde um D`);
+// } else if (nota >= 50) {
+//     console.log(`${nota} corresponde um F`)
+// };
